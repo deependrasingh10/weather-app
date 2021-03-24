b1df7f7ea930990a9aadd2ed95afb43dbae24625
deependrasingh10 <deependraks17@gmail.com>
2021-03-24 22:01:29 +0530

    Facbook-logIn

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props); 
+    this.state = {
+      buttonName: "Login"
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+	componentDidMount() {
+	  window.fbAsyncInit = function() {
+	    window.FB.init({
+	      appId      : '233405661863175',
+	      cookie     : true,
+	      xfbml      : true, 
+	      version    : 'v2.0'
+	    });
+	    window.FB.getLoginStatus(function(response) {
+	      this.statusChangeCallback(response);
+	    }.bind(this));
+	  }.bind(this);
+
+	  (function(d, s, id) {
+	    var js, fjs = d.getElementsByTagName(s)[0];
+	    if (d.getElementById(id)) return;
+	    js = d.createElement(s); js.id = id;
+	    js.src = "//connect.facebook.net/en_US/sdk.js";
+	    fjs.parentNode.insertBefore(js, fjs);
+	  }(document, 'script', 'facebook-jssdk'));
+	}
+
+	statusChangeCallback(response) {
+	  console.log('statusChangeCallback');
+
+	  if (response.status === 'connected') {
+	    console.log('User logged in successfully');
+      this.setState({ buttonName: "Logout" });
+      console.log(JSON.stringify(response));
+	  } else if (response.status === 'not_authorized') {
+	    console.log('Please log into weather app.');
+	  } else {
+	    console.log('Please log into Facebook.');
+	  }
+	}
+
+	checkLoginState() {
+	  window.FB.getLoginStatus(function(response) {
+	    this.statusChangeCallback(response);
+	  }.bind(this));
+	}
+
+	handleClick() {
+	  window.FB.login(this.checkLoginState());
+	}
+
+  render() {
+    return (
+      <div className="App">
+        <a href="#" onClick={this.handleClick}>{this.state.buttonName}</a>
+      </div>
+    );
+  }
 }
 
 export default App;
